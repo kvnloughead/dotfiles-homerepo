590ac57e1add391bc9189e3bb698d8577facf2f4
Kevin Loughead <kvnloughead@gmail.com>
2021-12-28 18:30:39 -0500

    wrap ssh commands in function to prevent proliferation of agents

--- dotfiles/system/ssh.sh
+++ dotfiles/system/ssh.sh
@@ -1,2 +1,6 @@
-eval "$(ssh-agent -s)" >/dev/null
-ssh-add ~/.ssh/id_ed25519  2>/dev/null
+loadssh() {
+	eval "$(ssh-agent -s)" >/dev/null
+	ssh-add ~/.ssh/id_ed25519  2>/dev/null
+}
+
+alias ssh="loadssh && ssh"
