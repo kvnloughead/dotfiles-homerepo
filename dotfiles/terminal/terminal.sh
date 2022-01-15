b4682c5926ac1ddadf09f69314cfdc875f219067
Kevin Loughead <kvnloughead@gmail.com>
2022-01-15 13:49:06 -0500

    Improve terminal config by loading windows user directory as bash variable

--- dotfiles/terminal/terminal.sh
+++ dotfiles/terminal/terminal.sh
@@ -1,3 +1,11 @@
-export terminalconfig="/mnt/c/Users/Kevin/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+# To open actual terminal config file, run `code $terminalconfig`
+# To copy actual terminal config to dotfiles repo, run `get_terminal_config`
 
-alias get-terminal-config="cp $terminalconfig ~/dotfiles/terminal/settings.json"
+winuser=`cmd.exe /c "echo %USERPROFILE%" 2> /dev/null | tr -d '\r'`
+winuser=`wslpath "${winuser}"`
+restofpath='/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json'
+terminalconfig="${winuser}${restofpath}"
+
+get_terminal_config() {
+	cp $terminalconfig ~/dotfiles/terminal/settings.json
+}
